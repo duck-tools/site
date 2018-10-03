7f692c0d1c54f1201ed31e864ffdda6b261e2760
Faeron <animeniac@live.com>
2018-10-03 15:19:25 -0600

    Add basic hello world response to server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res) => res.send('Hello, World!'));
+
+app.listen(process.env.PORT || 3000, () => console.log('App started'));
