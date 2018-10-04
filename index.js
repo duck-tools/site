a0edcac0ce0b34c3856562389029dc6f7fb79d41
Faeron <animeniac@live.com>
2018-10-04 12:24:05 -0600

    Change message to test auto deploy

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => res.send('Hello, World!'));
+app.use((req, res) => res.send('Hello, Heroku!'));
 
 app.listen(process.env.PORT || 3000, () => console.log('App started'));
