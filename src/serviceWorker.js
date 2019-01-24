75160b6d436e0c50d346c79d531c13e4eebb1b02
Faeron <animeniac@live.com>
2019-01-24 13:42:21 -0700

    Fix service worker url

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -62,7 +62,7 @@ function checkValidServiceWorker(swUrl, config) {
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL || '', window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
