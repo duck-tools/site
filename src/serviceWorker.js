3798238dfd1b8ad798b0aab4a3ca096a5f91890c
Faeron <animeniac@live.com>
2019-01-24 13:38:56 -0700

    Fix service worker url

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -68,7 +68,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL || 'http://localhost:3000'}/service-worker.js`;
+      const swUrl = `${publicUrl || 'http://localhost:3000'}/service-worker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
