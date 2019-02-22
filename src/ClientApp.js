d2a373543dc4918f1f236c96733c9c994d2f9dbf
Faeron <animeniac@live.com>
2019-02-22 15:22:53 -0700

    Disable concurrent mode

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { createRoot, render } from 'react-dom';
+import { unstable_createRoot, render } from 'react-dom';
 import App from './App';
 import { register, unregister } from './serviceWorker';
 
 const root = document.getElementById('root');
 const appData = JSON.parse(root.dataset.app);
 
-function ServerApp({ enableStrict }) {
-  const body = (
-    <App {...appData} />
-  );
-
+function UseStrict({ children, enableStrict }) {
   return enableStrict
-    ? <React.StrictMode><React.ConcurrentMode>{body}</React.ConcurrentMode></React.StrictMode>
-    : body;
+    ? <React.StrictMode>{ children }</React.StrictMode>
+    : <>{ children }</>;
 }
 
-//render(<ServerApp />, root);
-createRoot(root).render(<ServerApp />);
+function UseConcurrent({ children, enableConcurrent }) {
+  return enableConcurrent
+    ? <React.unstable_ConcurrentMode>{ children }</React.unstable_ConcurrentMode>
+    : <>{ children }</>;
+}
+
+function ServerApp({ enableStrict = false, enableConcurrent = false }) {
+  return (
+    <UseStrict enableStrict={enableStrict}>
+      <UseConcurrent enableConcurrent={enableConcurrent}>
+        <App {...appData} />
+      </UseConcurrent>
+    </UseStrict>
+  );
+}
+
+render(<ServerApp />, root);
+// unstable_createRoot(root).render(<ServerApp enableConcurrent={false} enableStrict={false} />);
 
 if (process.env.NODE_ENV === 'production') {
   register();
